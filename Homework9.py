757d92fd5a31fd0767ea093d7acbc711ea562a16
AnzhelikaKodlubovska <angelikagymnasticschannel@gmail.com>
2023-12-20 17:21:23 +0200

    Homework9

--- Homework9.py
+++ Homework9.py
@@ -10,30 +10,13 @@ def input_error(func):
             return "Insufficient information provided."
     return wrapper
     
-def saving(name, phone, dict_user):
-    dict_user[name] = phone
-    return dict_user
-    
-def change_phone_number(name, new_phone, dict_user):
-    if name in dict_user:
-        dict_user[name] = new_phone
-    return dict_user
-
-def show_phone_number(name, dict_user):
-    if name in dict_user:
-        print(f"Phone number for {name}: {dict_user[name]}")
-    else:
-        print(f"Contact {name} not found.")
-
-def show_all_contacts(dict_user):
-    if dict_user:
-        for name, phone in dict_user.items():
-            print(f"{name}: {phone}")
-
 @input_error
 def saving(name, phone, dict_user):
-    dict_user[name] = phone
-    return dict_user
+    if name not in dict_user:
+        dict_user[name] = phone
+        return f"Contact {name} added."
+    else:
+        return f"Contact {name} already exists. Use 'change' command to update the phone number."
 
 @input_error
 def change_phone_number(name, new_phone, dict_user):
@@ -41,7 +24,7 @@ def change_phone_number(name, new_phone, dict_user):
         dict_user[name] = new_phone
         return f"Phone number for {name} updated."
     else:
-        return f"Contact {name} not found."
+        return f"Contact {name} not found. Use 'add' command to create a new contact."
 
 @input_error
 def show_phone_number(name, dict_user):
@@ -53,32 +36,46 @@ def show_phone_number(name, dict_user):
 def main():
     dict_user = {}  
     while True:
-        question = input('>> ').lower()
-        if question == 'hello':
-            print('How can I help you?')
-        elif question.startswith('add'):
-            data = question.split()[1:]  
-            if len(data) == 2:
-                name, phone = data
-                dict_user = saving(name, phone, dict_user)
-            else:
-                print("Invalid input format. Please use 'add name phone'.")
-        elif question.startswith('change'):
-            data = question.split()[1:]  
-            if len(data) == 2:
-                name, new_phone = data
-                dict_user = change_phone_number(name, new_phone, dict_user)
-            else:
-                print("Invalid input format. Please use 'change name new_phone'.")
-        elif question.startswith('phone'):
-            name = question.split(' ', 1)[1] 
-            show_phone_number(name, dict_user)
-        elif question == 'show all':
-            show_all_contacts(dict_user)
-        elif question == '.' or question == "good bye" or question == "close" or question == "exit":
-            break
+        question = input('>> ').lower().strip()
+
+        known_commands = ['hello', 'add', 'change', 'phone', 'show all', '.', 'good bye', 'close', 'exit']
+        command = question.split()[0] if question else ''
+        
+        if command in known_commands or question == 'show all': 
+            commands = {
+                'hello': lambda: print('How can I help you?'),
+                'add': lambda data: saving(data[0], data[1], dict_user) if len(data) == 2 and data[0].isalpha() else print("Invalid input format. Please use 'add name phone'."),
+                'change': lambda data: change_phone_number(data[0], data[1], dict_user) if len(data) == 2 and data[0] in dict_user else print("Contact not found. Use 'add' command to create a new contact."),
+                'phone': lambda data: print(show_phone_number(' '.join(data), dict_user)) if ' '.join(data) in dict_user else print(f"Contact {' '.join(data)} not found."),
+                'show all': lambda: [print(f"{name}: {phone}") for name, phone in dict_user.items()] if dict_user else print("No contacts available."),
+                '.': lambda: (print('Good bye!'), exit()),
+                'good bye': lambda: (print('Good bye!'), exit()),
+                'close': lambda: (print('Good bye!'), exit()),
+                'exit': lambda: (print('Good bye!'), exit()),
+            }
+
+            try: 
+            
+                if command.startswith('add'):
+                    data = question.split()[1:]
+                    commands['add'](data)
+                elif command.startswith('change'):
+                    data = question.split()[1:]
+                    commands['change'](data)
+                elif command.startswith('phone'):
+                    data = question.split()[1:]
+                    commands['phone'](data)
+                elif command in commands:
+                    commands[command]()
+                elif question == 'show all':  
+                    commands['show all']()
+                else:
+                    print("Sorry, I didn't understand your request.")
+            except IndexError:
+                print("Insufficient information provided. Try again")
+                
         else:
-            print("Sorry, I didn't understand your request.")
+            print("Invalid command. Please enter a valid command.")
     
 if __name__ == '__main__':
     main()
